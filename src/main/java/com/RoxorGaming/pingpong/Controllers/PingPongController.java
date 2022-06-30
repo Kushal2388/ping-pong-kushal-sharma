b800b11377aa293b809ea53d7347e297a2c97a08
Kushal Sharma <kushal.sharma231988@gmail.com>
2022-06-30 14:37:22 +0100

    Changes in controller and test classes

--- src/main/java/com/RoxorGaming/pingpong/Controllers/PingPongController.java
+++ src/main/java/com/RoxorGaming/pingpong/Controllers/PingPongController.java
@@ -1,13 +1,14 @@
 package com.RoxorGaming.pingpong.Controllers;
 
-
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+//controller class
 @RestController
 public class PingPongController {
 
-    @RequestMapping("/ping")
+    @RequestMapping(value = "/ping", method = RequestMethod.GET)
     public String printPong(){
         return "pong";
     }
--- src/test/java/com/RoxorGaming/pingpong/PingPongApplicationTests.java
+++ src/test/java/com/RoxorGaming/pingpong/PingPongApplicationTests.java
@@ -1,13 +1,33 @@
 package com.RoxorGaming.pingpong;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class PingPongApplicationTests {
 
+	@LocalServerPort
+	private int port;
+
+	@Autowired
+	private TestRestTemplate testRestTemplate;
+
 	@Test
-	void contextLoads() {
+	void contextLoads(){
+
 	}
+
+	@Test
+	public void controllerMethodShouldReturnDefaultMessage(){
+		assertThat(this.testRestTemplate.getForObject("http://localhost:"+port+"/ping",String.class))
+				.contains("pong");
+	}
+
+
 
 }
